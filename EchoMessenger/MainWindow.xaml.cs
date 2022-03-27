75ce1c062e90337f3b315a9a7fb35c448f2dea16
tyulyukov <makstyulyukov@gmail.com>
2022-03-27 02:33:08 +0200

    Fluid design + Sending messages

--- EchoMessenger/MainWindow.xaml.cs
+++ EchoMessenger/MainWindow.xaml.cs
@@ -24,5 +24,44 @@ namespace EchoMessenger
         {
             InitializeComponent();
         }
+
+        private void SendMessageButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(MessageTextBox.Text))
+                return;
+
+            var message = CreateMyMessage(MessageTextBox.Text);
+            MessageTextBox.Text = String.Empty;
+
+            MessagesStackPanel.Children.Add(message);
+            MessagesScroll.ScrollToBottom();
+        }
+
+        // NOTE: Create a fabric of UIElements
+        private UIElement CreateMyMessage(String text)
+        {
+            var border = new Border();
+            border.MinHeight = 50;
+            border.MaxWidth = 500;
+            border.BorderBrush = new SolidColorBrush(Colors.White);
+            border.BorderThickness = new Thickness(1);
+            border.Margin = new Thickness(10);
+            border.HorizontalAlignment = HorizontalAlignment.Right;
+            border.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#283EFF");
+            border.CornerRadius = new CornerRadius(30);
+
+            var textBlock = new TextBlock();
+            textBlock.Text = text;
+            textBlock.TextWrapping = TextWrapping.Wrap;
+            textBlock.VerticalAlignment = VerticalAlignment.Center;
+            textBlock.Foreground = new SolidColorBrush(Colors.White);
+            textBlock.FontSize = 14;
+            textBlock.HorizontalAlignment = HorizontalAlignment.Left;
+            textBlock.Margin = new Thickness(10);
+
+            border.Child = textBlock;
+
+            return border;
+        }
     }
 }
