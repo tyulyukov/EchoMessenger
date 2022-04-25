ebc74c36d238c9d2b2af4a27d3bc82441bdb1e82
tyulyukov <makstyulyukov@gmail.com>
2022-04-25 16:18:10 +0300

    Smooth progress bar in Search tab

---
+++ EchoMessenger/Helpers/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace EchoMessenger.Helpers
+{
+    public static class Extensions
+    {
+        public static void SetPercent(this ProgressBar progressBar, double percentage, TimeSpan duration)
+        {
+            DoubleAnimation animation = new DoubleAnimation(percentage, duration);
+            progressBar.BeginAnimation(ProgressBar.ValueProperty, animation);
+        }
+    }
+}
--- EchoMessenger/Views/SearchView.xaml.cs
+++ EchoMessenger/Views/SearchView.xaml.cs
@@ -3,7 +3,7 @@ using EchoMessenger.Helpers;
 using Firebase.Database;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 
@@ -14,27 +14,35 @@ namespace EchoMessenger.Views
     /// </summary>
     public partial class SearchView : UserControl
     {
+        private bool isSearching = false;
+        private double progress = 0;
+        private SynchronizationContext? uiSync;
+
         public SearchView()
         {
             InitializeComponent();
+            uiSync = SynchronizationContext.Current;
         }
 
         private async void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
+            UsersStackPanel.Children.Clear();
+
             TextBox? textBox = sender as TextBox;
 
             if (textBox == null || String.IsNullOrWhiteSpace(textBox.Text))
                 return;
 
+            _ = Task.Run(StartFillingProgressBar);
             var users = await SearchUsers(textBox.Text);
-            
-            UsersStackPanel.Children.Clear();
+            _ = Task.Run(EndFillingProgressBar);
 
             if (users == null)
                 return;
 
             foreach (var user in users)
                 UsersStackPanel.Children.Add(UIElementsFactory.CreateUsersCard(user.Object.AvatarUrl, user.Object.Name));
+            
         }
 
         private async Task<IEnumerable<FirebaseObject<User>>?> SearchUsers(String contains)
@@ -44,7 +52,47 @@ namespace EchoMessenger.Views
 
         private void ButtonSearch_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            SearchTextBox_TextChanged(SearchTextBox, null);
+            SearchTextBox_TextChanged(SearchTextBox, (TextChangedEventArgs)EventArgs.Empty);
+        }
+
+        private void StartFillingProgressBar()
+        {
+            isSearching = true;
+            SetProgress(0, TimeSpan.FromMilliseconds(100));
+
+            while (progress != 95)
+            {
+                if (!isSearching)
+                    break;
+
+                SetProgress(progress + 1, TimeSpan.FromMilliseconds(50));
+
+                if (progress < 50)
+                    Thread.Sleep(50);
+                else if (progress < 75)
+                    Thread.Sleep(500);
+                else if (progress < 90)
+                    Thread.Sleep(1500);
+                else if (progress < 95)
+                    Thread.Sleep(5000);
+                else break;
+            }
+        }
+
+        private void SetProgress(double progress, TimeSpan duration)
+        {
+            this.progress = progress;
+            uiSync?.Send(state => { SearchProgressBar.SetPercent((double)state, duration); }, progress);
+        }
+
+        private void EndFillingProgressBar()
+        {
+            isSearching = false;
+            double delay = 100 - progress;
+
+            SetProgress(100, TimeSpan.FromMilliseconds(delay));
+            Thread.Sleep((int)delay);
+            SetProgress(0, TimeSpan.FromMilliseconds(100));
         }
     }
 }
